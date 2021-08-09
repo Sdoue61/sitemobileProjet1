ceee2b905b28aefa539d4f8f6917b6bfe4b9d2fc
Sdoue61 <sebastiendoue@gmail.com>
2021-08-09 11:31:24 +0400

    SebastienProjet1-2

--- app/src/main/java/com/example/projet12/MainActivity2.kt
+++ app/src/main/java/com/example/projet12/MainActivity2.kt
@@ -1,66 +1,66 @@
 package com.example.projet12
 
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
-    var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val ans1 = findViewById<Button>(R.id.bt1)
-        val ans2 = findViewById<Button>(R.id.bt2)
-        val ans3 = findViewById<Button>(R.id.bt3)
-        val ans4 = findViewById<Button>(R.id.bt4)
+        val ans1 = findViewById<Button>(R.id.ans1)
+        val ans2 = findViewById<Button>(R.id.ans2)
+        val ans3 = findViewById<Button>(R.id.ans3)
+        val ans4 = findViewById<Button>(R.id.ans4)
         val resultat1 = findViewById<TextView>(R.id.TV_result)
+
+        val intent = Intent(this, MainActivity3::class.java)
+
         val builder = AlertDialog.Builder(this)
 
-            ans1.setOnClickListener() {
-                builder.setTitle("PERDU")
-                builder.setMessage("La reponse etait Paris!")
-                builder.setPositiveButton("Question Suivante", null)
+        var counter = 0
+        counter += 0
+        resultat1.text = counter.toString()
+
+        val myIntent = Intent(this, MainActivity3::class.java)
+        myIntent.putExtra("compteur", counter)
+
+        builder.setTitle("PERDU")
+        builder.setMessage("La reponse etait Paris!")
+        builder.setPositiveButton("Question Suivante") { _: DialogInterface, _: Int ->
+            startActivity(myIntent)
+            startActivity(intent)
+        }
+
+            ans1.setOnClickListener {
                 builder.show()
-                counter += 1
-                resultat1.text = counter.toString()
             }
 
-            ans2.setOnClickListener() {
+            ans2.setOnClickListener {
+                counter += 1
+                resultat1.text = counter.toString()
                 builder.setTitle("BRAVO")
                 builder.setMessage("Tu es un genie!")
-                builder.setPositiveButton("Question Suivante", null)
+                builder.setPositiveButton("Question Suivante") { _: DialogInterface, _: Int ->
+                    startActivity(myIntent)
+                    startActivity(intent)
+                }
                 builder.show()
-                counter += 1
-                resultat1.text = counter.toString()
             }
 
-            ans3.setOnClickListener() {
-                builder.setTitle("PERDU")
-                builder.setMessage("La reponse etait Paris!")
-                builder.setPositiveButton("Question Suivante", null)
+            ans3.setOnClickListener {
                 builder.show()
-                counter += 1
-                resultat1.text = counter.toString()
             }
 
-            ans4.setOnClickListener() {
-                builder.setTitle("PERDU")
-                builder.setMessage("La reponse etait Paris!")
-                builder.setPositiveButton("Question Suivante", null)
+            ans4.setOnClickListener {
                 builder.show()
-                counter += 1
-                resultat1.text = counter.toString()
             }
-        val next = findViewById<Button>(R.id.bt_suivant1)
-        next.setOnClickListener{
-            val intent = Intent(this, MainActivity3::class.java)
-            startActivity(intent)
-            finish()
-        }
     }
 }
 
--- app/src/main/java/com/example/projet12/MainActivity3.kt
+++ app/src/main/java/com/example/projet12/MainActivity3.kt
@@ -1,65 +1,58 @@
 package com.example.projet12
 
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity3 : AppCompatActivity() {
-    var counter2 = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
 
-        val ans1_2 = findViewById<Button>(R.id.page3bt1)
-        val ans2_2 = findViewById<Button>(R.id.page3bt2)
-        val ans3_2 = findViewById<Button>(R.id.page3bt3)
-        val ans4_2 = findViewById<Button>(R.id.page3bt4)
+        val ans1 = findViewById<Button>(R.id.ans1_2)
+        val ans2 = findViewById<Button>(R.id.ans2_2)
+        val ans3 = findViewById<Button>(R.id.ans3_2)
+        val ans4 = findViewById<Button>(R.id.ans4_2)
         val resultat2 = findViewById<TextView>(R.id.TV_result_2)
-        val builder_2 = AlertDialog.Builder(this)
 
-        ans1_2.setOnClickListener() {
-            builder_2.setTitle("PERDU")
-            builder_2.setMessage("La reponse etait Rome!")
-            builder_2.setPositiveButton("Question Suivante", null)
-            builder_2.show()
-            counter2 += 1
+        val intent = Intent(this, MainActivity::class.java)
+
+        var counter2 = getIntent().getIntExtra("compteur", 0)
             resultat2.text = counter2.toString()
+
+        val builder2 = AlertDialog.Builder(this)
+            builder2.setTitle("PERDU")
+            builder2.setMessage("La reponse etait Rome!")
+            builder2.setPositiveButton("Recommencez le jeu") { _: DialogInterface, _: Int ->
+                startActivity(intent)
         }
 
-        ans2_2.setOnClickListener() {
-            builder_2.setTitle("PERDU")
-            builder_2.setMessage("La reponse etait Rome!")
-            builder_2.setPositiveButton("Question Suivante", null)
-            builder_2.show()
-            counter2 += 1
-            resultat2.text = counter2.toString()
+        ans1.setOnClickListener {
+            builder2.show()
         }
 
-        ans3_2.setOnClickListener() {
-            builder_2.setTitle("BRAVO")
-            builder_2.setMessage("Tu es un genie!")
-            builder_2.setPositiveButton("Question Suivante", null)
-            builder_2.show()
-            counter2 += 1
-            resultat2.text = counter2.toString()
+        ans2.setOnClickListener {
+            builder2.show()
         }
 
-        ans4_2.setOnClickListener() {
-            builder_2.setTitle("PERDU")
-            builder_2.setMessage("La reponse etait Rome!")
-            builder_2.setPositiveButton("Question Suivante", null)
-            builder_2.show()
+        ans3.setOnClickListener {
             counter2 += 1
             resultat2.text = counter2.toString()
+            builder2.setTitle("BRAVO")
+            builder2.setMessage("Tu es un genie!")
+            builder2.setPositiveButton("Recommencez le jeu") { _: DialogInterface, _: Int ->
+                startActivity(intent)
+            }
+            builder2.show()
         }
-        val next = findViewById<Button>(R.id.bt_retour)
-        next.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+
+        ans4.setOnClickListener {
+            builder2.show()
         }
     }
 }
